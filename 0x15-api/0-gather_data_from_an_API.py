2905bdd2ede08062e9bb3b5909530b299cf82ef4
Xys-Tus <Sixtus Onyedibe>
2023-07-24 16:02:46 +0100

    Added a try and except to catch error due to wrong data type

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,20 +2,23 @@
 import requests
 
 if __name__ == "__main__":
-    employee_Id = int(input())
-    User_Data_Info = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(employee_Id))
-    TOdos_response = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'.format(employee_Id))
-    ListOfTodos = TOdos_response.json()
-    UserInfo = User_Data_Info.json()
-    Current_Employee_name = UserInfo['name']
-    totalNumberOfTasks = len(ListOfTodos)
-    numberOfCompletedTask = 0
-    List_of_completed_tasks = []
-    for i in ListOfTodos:
-        List_of_completed_tasks.append(i['title'])
-        if i['completed'] == True:
-            numberOfCompletedTask = numberOfCompletedTask + 1
-    print('Employee {} is done with tasks({}/{})'.format(Current_Employee_name, numberOfCompletedTask, totalNumberOfTasks ))
-    for completedTask in List_of_completed_tasks:
-        print(completedTask)
+    try:
+        employee_Id = int(input())
+        User_Data_Info = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(employee_Id))
+        TOdos_response = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'.format(employee_Id))
+        ListOfTodos = TOdos_response.json()
+        UserInfo = User_Data_Info.json()
+        Current_Employee_name = UserInfo['name']
+        totalNumberOfTasks = len(ListOfTodos)
+        numberOfCompletedTask = 0
+        List_of_completed_tasks = []
+        for i in ListOfTodos:
+            List_of_completed_tasks.append(i['title'])
+            if i['completed'] == True:
+                numberOfCompletedTask = numberOfCompletedTask + 1
+        print('Employee {} is done with tasks({}/{})'.format(Current_Employee_name, numberOfCompletedTask, totalNumberOfTasks ))
+        for completedTask in List_of_completed_tasks:
+            print(completedTask)
+    except:
+        print("Please enter an Integer")
 
